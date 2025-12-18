5868d8a963a59b2231d0f84a1bec4f73dc1c58d7
Kyle Willett <kylewillett@yahoo.com>
2025-12-18 14:36:34 -0600

    Added initial routes and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
+import Home from "./pages/Home"
+import Layout from "./components/Layout"
 
 function App() {
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
   )
 }
+
+/*
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+*/
 
 export default App
