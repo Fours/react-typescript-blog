9eea2360ee570b52b4e3fe211a60c19e16aa6809
Kyle Willett <kylewillett@yahoo.com>
2025-12-22 19:57:47 -0600

    Added list to authors page

--- src/pages/Authors.tsx
+++ src/pages/Authors.tsx
@@ -1,3 +1,21 @@
+import usePromise from "../hooks/usePromise";
+import { getAllAuthors } from "../services/articlesService";
+
 export default function Authors() {
-    return <h1>Authors page</h1>
+
+    const { result, isLoading, isError } = usePromise(getAllAuthors)
+    const authors = result ? result : []
+
+    return (
+        <>
+            <h1>Authors</h1>
+            {isLoading ? <p>Loading...</p> : null}
+            {isError ? <p>Whoops, something went wrong. Please try again.</p> : null}
+            {result ? <div>
+                {authors.map(author => {
+                    return <h3 key={author}>{author}</h3>
+                })}
+            </div> : null}
+        </>
+    )
 }
--- src/services/articlesService.ts
+++ src/services/articlesService.ts
@@ -5,11 +5,21 @@ const host = "http://localhost:8000"
 
 export async function getAllArticles(): Promise<Article[]> {
 
-    await delay(1000)
+    //await delay(1000)
     const res = await fetch(`${host}/api/articles`)
     const data = await res.json()
     return data as unknown as Article[]
 }
+
+export async function getAllAuthors(): Promise<string[]> {
+
+    //await delay(1000)
+    const res = await fetch(`${host}/api/authors`)
+    const data = await res.json()
+    return data as unknown as string[]
+}
+
+
 
 function delay(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
